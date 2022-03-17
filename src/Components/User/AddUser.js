f8e1fe3c4a5b666e094e6d7f6a986c286cf66e90
PatrekGill <patrekGill@yahoo.com>
2022-03-17 11:57:53 -0600

    finished section 8

--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -1,41 +1,81 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
+import classes from "./AddUser.module.css";
 
-const AddUser = props => {
-	const [newUserName, setNewUserName] = useState("");
-	const [newAge, setNewAge] = useState("");
+const AddUser = (props) => {
+    const [newUserName, setNewUserName] = useState("");
+    const [newAge, setNewAge] = useState("");
+	const [error, setError] = useState("");
 
-	const addUserHandler = (event) => {
-		event.preventDefault();
+    const addUserHandler = (event) => {
+        event.preventDefault();
 
-		const user = {
-			id: Math.random(),
-			userName: newUserName,
-			age: +newAge
-		};
+        if (newUserName.trim() === "") {
+			setError({
+				title: "Invlid Username",
+				message: "Username can't be blank!"
+			});
+            return;
+        }
 
-		props.onAddUser(user);
-		setNewUserName("");
-		setNewAge("");
+        if (+newAge < 1) {
+			setError({
+				title: "Invlid Age",
+				message: "Age must be more then 1!"
+			});
+            return;
+        }
+
+        const user = {
+            id: Math.random(),
+            userName: newUserName,
+            age: +newAge,
+        };
+
+        props.onAddUser(user);
+        setNewUserName("");
+        setNewAge("");
+    };
+
+    const changedUserNameHandler = (event) => {
+        setNewUserName(event.target.value);
+    };
+    const changedAgeHandler = (event) => {
+        setNewAge(event.target.value);
+    };
+
+	const errorHandler = () => {
+		setError(null);
 	};
 
-	const changedUserNameHandler = event => {
-		setNewUserName(event.target.value);
-	};
-	const changedAgeHandler = event => {
-		setNewAge(event.target.value);
-	};
+    return (
+        <div>
+            {error && <ErrorModal onErrorReset={errorHandler} title={error.title} message={error.message}/>}
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">UserName</label>
+                    <input
+                        value={newUserName}
+                        type="text"
+                        id="username"
+                        onChange={changedUserNameHandler}
+                    />
 
-	return (
-		<form onSubmit={addUserHandler}>
-			<label htmlFor="username">UserName</label>
-			<input type="text" id="username" onChange={changedUserNameHandler} />
+                    <label htmlFor="age">Age (Years</label>
+                    <input
+                        value={newAge}
+                        type="number"
+                        id="age"
+                        onChange={changedAgeHandler}
+                    />
 
-			<label htmlFor="age">Age</label>
-			<input type="number" id="age" onChange={changedAgeHandler}/>
-
-			<button type="submit">AddUser</button>
-		</form>
-	);
+                    <Button type="submit">AddUser</Button>
+                </form>
+            </Card>
+        </div>
+    );
 };
 
 export default AddUser;
--- src/Components/User/UsersList.js
+++ src/Components/User/UsersList.js
@@ -1,17 +1,21 @@
+import Card from "../UI/Card";
+import classes from './UsersList.module.css'
+
 const UsersList = (props) => {
-	console.log(props.items);
-	return (
-		<div>
-			{
-				props.items.map((item) => (
-					<div key={item.id}>
-						{item.userName}
-						{item.age}
-					</div>
-				))
-			}
-		</div>
-	);
+    console.log(props.items);
+    return (
+        <Card className={classes.users}>
+            <ul>
+                {props.items.map((user) => (
+                    <li key={user.id}>
+                        {user.userName}
+						:
+                        {user.age}
+                    </li>
+                ))}
+            </ul>
+        </Card>
+    );
 };
 
 export default UsersList;

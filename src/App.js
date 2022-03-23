2f7a74c2756a3daa30a6a4a03e0e0857a162a5b3
PatrekGill <patrekGill@yahoo.com>
2022-03-23 12:22:06 -0600

    started on section 14

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React, {useState} from "react";
 
+import MoviesList from "./components/MoviesList";
 import "./App.css";
-import DemoList from "./components/Demo/DemoList";
-import Button from "./components/UI/Button/Button";
 
 function App() {
-    const [listTitle, setListTitle] = useState("My List");
+    const dummyMovies = [
+        {
+            id: 1,
+            title: "Some Dummy Movie",
+            openingText: "This is the opening text of the movie",
+            releaseDate: "2021-05-18",
+        },
+        {
+            id: 2,
+            title: "Some Dummy Movie 2",
+            openingText: "This is the second opening text of the movie",
+            releaseDate: "2021-05-19",
+        },
+    ];
 
-    const changeTitleHandler = useCallback(() => {
-        setListTitle("New Title");
-    }, []);
+	const [movies,setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-    const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+	// First HTTP syntax
+
+	// function fetchMoviesHandler() {
+	// 	fetch('https://swapi.dev/api/films/').then(response => {
+	// 		return response.json();
+	// 	}).then(data => {
+	// 		const transformedMovies = data.results.map((movie) => {
+	// 			return {
+	// 				id: movie.episode_id,
+	// 				title: movie.title,
+	// 				openingText: movie.opening_crawl,
+	// 				releaseDate: movie.release_date
+	// 			}
+	// 		});
+
+	// 		setMovies(transformedMovies);
+	// 	});
+	// };
+
+	// await syntax
+	async function fetchMoviesHandler() {
+		setIsLoading(true);
+
+		const response = await fetch('https://swapi.dev/api/films/');
+		const data = await response.json();
+
+		const transformedMovies = data.results.map((movie) => {
+			return {
+				id: movie.episode_id,
+				title: movie.title,
+				openingText: movie.opening_crawl,
+				releaseDate: movie.release_date
+			}
+		});
+
+		setMovies(transformedMovies);
+		setIsLoading(false);
+	};
 
     return (
-        <div className="app">
-            <DemoList title={listTitle} items={listItems} />
-            <Button onClick={changeTitleHandler}>Change List Title</Button>
-        </div>
+        <React.Fragment>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>
+                {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+				{!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+				{isLoading && <p>Loading...</p>}
+            </section>
+        </React.Fragment>
     );
 }
 

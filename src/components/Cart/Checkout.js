28e85fd2b44047cdaa2f9b9c786ff3ed5955ca4b
PatrekGill <patrekGill@yahoo.com>
2022-03-28 13:31:13 -0600

    completed section 17

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,6 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
+const isEmpty = (value) => value.trim() === "";
+const isFiverChars = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
+    const [formInputsValidity, setFormInputsValidity] = useState({
+        name: true,
+        street: true,
+        city: true,
+        postaCode: true,
+    });
+
     const nameRef = useRef("");
     const streetRef = useRef("");
     const postalCodeRef = useRef();
@@ -8,47 +18,75 @@ const Checkout = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
+        const enteredName = nameRef.current.value;
+        const enteredStreet = streetRef.current.value;
+        const enteredPostal = postalCodeRef.current.value;
+        const enteredCity = cityRef.current.value;
+
+        const nameIsValid = !isEmpty(enteredName);
+        const streetIsValid = !isEmpty(enteredStreet);
+        const cityIsValid = !isEmpty(enteredCity);
+        const postalIsValid = isFiverChars(enteredPostal);
+
+        setFormInputsValidity({
+            name: nameIsValid,
+            street: streetIsValid,
+            city: cityIsValid,
+            postaCode: postalIsValid,
+        });
+
+        const formIsValid =
+            nameIsValid && streetIsValid && postalIsValid && cityIsValid;
 
         // console.log(nameRef.current.value);
         // console.log(streetRef.current.value);
         // console.log(postalCodeRef.current.value);
         // console.log(cityRef.current.value);
+
+        if (!formIsValid) {
+            return;
+        }
+
         const userDetails = {
-            name: nameRef.current.value,
-            street: streetRef.current.value,
-            postal: postalCodeRef.current.value,
-            city: cityRef.current.value,
+            name: enteredName,
+            street: enteredStreet,
+            postal: enteredPostal,
+            city: enteredCity,
         };
 
-		props.onSubmitOrder(userDetails);
+        props.onSubmitOrder(userDetails);
     };
 
-	const cancelOrderHandler = () => {
-		nameRef.current.value = "";
-		streetRef.current.value = "";
-		postalCodeRef.current.value = "";
-		cityRef.current.value = "";
+    const cancelOrderHandler = () => {
+        nameRef.current.value = "";
+        streetRef.current.value = "";
+        postalCodeRef.current.value = "";
+        cityRef.current.value = "";
 
-		props.onCancelOrder();
-	};
+        props.onCancelOrder();
+    };
 
     return (
         <form onSubmit={submitHandler}>
             <div>
                 <label htmlFor="name">Full Name</label>
                 <input id="name" type="text" ref={nameRef} />
+				{!formInputsValidity.name && <p>Please enter a valid name.</p>}
             </div>
             <div>
                 <label htmlFor="street">Street Address</label>
                 <input id="street" type="text" ref={streetRef} />
+				{!formInputsValidity.street && <p>Please enter a valid street address.</p>}
             </div>
             <div>
                 <label htmlFor="postal">Postal Code</label>
                 <input id="postal" type="text" ref={postalCodeRef} />
+				{!formInputsValidity.postaCode && <p>Please enter a valid postal code.</p>}
             </div>
             <div>
                 <label htmlFor="city">City</label>
                 <input id="city" type="text" ref={cityRef} />
+				{!formInputsValidity.city && <p>Please enter a valid city.</p>}
             </div>
             <button onClick={cancelOrderHandler}>Cancel</button>
             <button>Confirm</button>

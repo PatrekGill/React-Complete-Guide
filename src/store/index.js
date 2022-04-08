50645d01d8d2a05d4f108adf856fc1c501597028
PatrekGill <patrekGill@yahoo.com>
2022-04-08 11:22:49 -0600

    up to 242

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,37 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+    name: "counter",
+    initialState,
+    reducers: {
+        increment(state) {
+			state.counter++;
+		},
+        decrement(state) {
+			state.counter--;
+		},
+        increase(state,action) {
+			state.counter = state.counter + action.amount;
+		},
+        toggleCounter(state) {state.showCounter = !state.showCounter},
+    },
+});
 
 const counterReducer = (state = initialState, action) => {
     if (action.type === "increment") {
         return {
             counter: state.counter + 1,
-			showCounter: state.showCounter
+            showCounter: state.showCounter,
         };
     }
 
     if (action.type === "increase") {
         return {
             counter: state.counter + action.amount,
-			showCounter: state.showCounter
+            showCounter: state.showCounter,
         };
     }
 
@@ -23,10 +41,10 @@ const counterReducer = (state = initialState, action) => {
         };
     }
 
-	if (action.type === "toggle") {
+    if (action.type === "toggle") {
         return {
-			counter: state.counter,
-			showCounter: !state.showCounter
+            counter: state.counter,
+            showCounter: !state.showCounter,
         };
     }
 
